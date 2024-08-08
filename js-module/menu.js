d5cb711ef3d63d7cf9a28e965d613597dd709506
DjCr4y0n <cray.kox1234@gmail.com>
2024-08-08 20:55:51 +0200

    Add a little bit of style to menu page just to end it and move on

--- js-module/menu.js
+++ js-module/menu.js
@@ -7,13 +7,18 @@ export function menuPage()
 
     const menuHeadline = document.createElement('h1');
     menuHeadline.textContent = "MENU";
+    const dishes = document.createElement('div');
+    dishes.classList.add('dishes')
     menuPage.appendChild(menuHeadline);
+    menuPage.appendChild(dishes);
 
 
     const numberOfDishes = 8;
     for (let index = 0; index < numberOfDishes; index++) {
         const position = document.createElement('div');
-        menuPage.appendChild(position);
+        position.textContent = "BAGNO";
+        position.style.color = "red";
+        dishes.appendChild(position);
 
 
     }
